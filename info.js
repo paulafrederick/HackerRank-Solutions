8e67dd655e1a643fbf31c3e97396d36f7c9d8cd4
rAwsam <sapphirerosam@gmail.com>
2020-10-28 20:24:45 +0530

    Update info.js
    XOXOXO

--- info.js
+++ info.js
@@ -3,6 +3,25 @@ name:"alphaCoder",
 problemLink:"https://www.hackerrank.com/challenges/java-anagrams/problem",
 language:"Java",
 solutionUrl:"./Solutions/anagram.java"
+<<<<<<< Updated upstream
 master
 
  master
+=======
+<<<<<<< HEAD
+},{
+    name:"raWsam",
+    problemLink:"https://www.hackerrank.com/challenges/c-sum_of_array",
+    language:"C",
+    solutionUrl:"./Solutions/anagram.java"
+    },{ name:"uknwwho",
+    problemLink:"https://www.hackerrank.com/challenges/c-multiplication_of_several_arrays",
+    language:"C",
+    solutionUrl:"./Solutions/anagram.java"
+    }]
+=======
+master
+
+ master
+>>>>>>> 2263d0bca7cbf87ce3e206cb8489e277152fef39
+>>>>>>> Stashed changes
